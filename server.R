0ed414870bb5b4ef9d0983259da0bb52abfdad43
bencuben <bcanou@unal.edu.co>
2019-07-28 10:56:28 -0500

    28/07/19   Primera versión de la aplicación super básica

---
+++ server.R
@@ -0,0 +1,102 @@
+
+library(lubridate)
+library(dplyr)
+library(ggplot2)
+library(ggplotAssist)
+library(shiny)
+library(shinythemes)
+
+#Open file####
+#68 lineas inservibles 
+datos <- read.table("www/AirSea_ST_0001.asc", skip = 68,header = T)
+#Fix the base####
+datos1 <- datos[datos$SSH != -99,c("YYYYMM","SSH")]
+datos1$YYYYMM <- ymd(datos1$YYYYMM,truncated = 2)
+datos1$Month <- month(datos1$YYYYMM)
+datos1$Year <- year(datos1$YYYYMM)
+datos1 <- datos1[,c("Year","Month","SSH")]
+#Means Year and Month####
+MY <- datos1 %>% group_by(Year) %>% summarise(medias = mean(SSH))
+MM <- datos1 %>% group_by(Month) %>% summarise(medias = mean(SSH))
+
+
+datos2 <- datos[datos$SSH != -99,c("SSH")]
+
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+   
+  output$plot1 <- renderPlot({
+    
+    
+    ggplot(data = MY, aes(x=Year, y=medias)) + 
+      geom_boxplot(data = datos1,aes(x=Year,y=SSH,group=Year), 
+                   fill = "#aee7e8", outlier.color = "#24009c") +
+      scale_y_continuous(name = "Height") + 
+      scale_x_continuous(name = "Year", breaks = seq(min(MY$Year),max(MY$Year),1)) + 
+      labs(tag = "Sea surface height") + 
+      theme(plot.tag = element_text(lineheight = 2,face = "bold",size = 20),
+            plot.tag.position = "top")  +
+      geom_point(color = "#c72c41") + geom_line(color = "#c72c41")
+    
+    
+    ggplot(data = MM, aes(x=Month, y=medias)) + 
+      geom_boxplot(data = datos1,aes(x=Month,y=SSH,group=Month), 
+                   fill = "#aee7e8", outlier.color = "#24009c") + 
+      scale_y_continuous(name = "Height") + 
+      scale_x_continuous(name = "Month", breaks = 1:12 , labels = 
+                           c("Ene","Feb","Mar","Abr","May","Jun","Jul","Ago","Sep",
+                             "Oct","Nov","Dic")) +
+      labs(tag = "Sea surface height") + 
+      theme(plot.tag = element_text(lineheight = 2,face = "bold",size = 20),
+            plot.tag.position = "top")  +
+      geom_point(color = "#c72c41")  + geom_line(color = "#c72c41")
+    
+  })
+  
+  
+  output$plot2 <- renderPlot({
+    
+    
+    
+    #Data analysis####
+    Quant <- data.frame(Probs=seq(0,1,0.0001),
+                        Cuantil_P=quantile(datos2,probs = seq(0,1,0.0001)))
+    
+    Ajuste <- fitDist(y = datos2, type = "realplus")
+    
+    histDist(datos2, family=as.name(Ajuste$family[1]), col.hist = "white", 
+             col.main = "Black", line.col = "blue",border.hist = "black",
+             main= paste("Family:",Ajuste$family[2]), xlab="Datos",ylab="Densidad",
+             col.axis = "black", col.lab = "black")
+    #eval = Evalua expresiones
+    #parse = convierte textos en expresiones sin evaluar
+    #paste0 = concatena textos sin espacios
+    Quant$Cuantil_T <- eval(parse(text = paste0("q",Ajuste$family[1],
+                                                "(p=seq(0,1,0.0001),mu=",
+                                                Ajuste$mu,
+                                                ",sigma=",Ajuste$sigma,")")))
+    #Graphic####
+    d_0.01 <- round(Quant$Cuantil_P[101],2)
+    d_0.05 <- round(Quant$Cuantil_P[501],2)
+    d_0.95 <- round(Quant$Cuantil_P[9501],2)
+    d_0.99 <- round(Quant$Cuantil_P[9901],2)
+    ggplot(data = Quant) + 
+      geom_line(aes(x=Cuantil_P,y=Probs), color = "#FF9770", size=1) +
+      geom_line(aes(x=Cuantil_T,y=Probs), color = "#70D6FF", size=1) + 
+      scale_x_continuous(name = "X",breaks = seq(0,ceiling(max(datos2)),5)) +
+      scale_y_continuous(name = "Probability") +
+      labs(tag = paste("Data VS",Ajuste$family[2])) +
+      theme(plot.tag = element_text(lineheight = 2,face = "bold",size = 15),
+            plot.tag.position = "top") +
+      scale_fill_manual(values = c("#FF9770","#70D6FF")) +
+      geom_text(aes(x=d_0.01,y=0.01),label=d_0.01)+
+      geom_text(aes(x=d_0.05,y=0.05),label=d_0.05)+
+      geom_text(aes(x=d_0.95,y=0.95),label=d_0.95)+
+      geom_text(aes(x=d_0.99,y=0.99),label=d_0.99)
+    
+    
+    
+  })
+  
+})
---
+++ ui.R
@@ -0,0 +1,74 @@
+library(lubridate)
+library(dplyr)
+library(ggplot2)
+library(ggplotAssist)
+library(shiny)
+library(shinythemes)
+# Define UI for application that draws a histogram
+shinyUI(
+  
+  
+  navbarPage(title = tags$b("Oceanic app"),id="navibar",
+             tabPanel(title="Pruebas",icon = icon("chart-line","fa-1x"),
+                      fluidPage(theme=shinytheme("cerulean"),
+                        
+                        sidebarLayout(
+                          sidebarPanel(
+                            fluidRow(
+                              column(7,img(src="escudo.png", height="100%", width="100%")),
+                              column(4,tags$p(tags$p(""),align="left"))
+                            ),
+                            
+                            
+                            fileInput("file1", "Carga tu base de datos",
+                                      multiple = FALSE,
+                                      accept = c("text/csv",
+                                                 "text/comma-separated-values,text/plain",
+                                                 ".csv")#,
+                                      #buttonLabel = "Explorar...",
+                                      #placeholder = "Ningún archivo"
+                            ),
+                            #Pregunta si los datos tienen encabezado o no
+                            
+                            tags$p("¿La base de datos tiene encabezado?"),
+                            
+                            checkboxInput("header", "Encabezado", FALSE),
+                            
+                            #Dos opciones en una sola fila
+                            
+                            fluidRow(
+                              
+                              #Pregunta el tipo de separador de los datos
+                              column(6,selectInput("sep", "Separador",width = "100%",
+                                                   choices = c("Coma" = ",",
+                                                               "Punto coma" = ";",
+                                                               "Tabular" = "\t"),
+                                                   selected = ",")
+                              ),
+                              #Pregunta el tipo de decimal usado en los datos
+                              column(5,selectInput("dec","Decimal",width = "100%",
+                                                   choices = c("Punto"=".",
+                                                               "Coma"=","),
+                                                   selected=",")
+                              )
+                              
+                            )
+                            
+                          ),
+                          
+                          # Show a plot of the generated distribution
+                          mainPanel(
+                            plotOutput("plot1"),
+                            plotOutput("plot2")
+                          )
+                        )
+                      )
+                      
+             ),#tab 1
+             tabPanel(title="descriotivo",icon = icon("chart-line","fa-1x"),
+                      tags$p("asdasdasdasdasd")
+             )#tab 2
+  )#NAVBAR
+  
+  
+)#UI
