3030b4e5b5a8720eb77a0324a75ce2f86c847aeb
WilliamAR <waguilarr@unal.edu.co>
2019-11-06 11:09:24 -0500

    Correccion: Nombre a los ejes, el fondo de las graficas y perimitir distribuciones a los reales

--- server.R
+++ server.R
@@ -138,12 +138,12 @@ shinyServer(function(input, output,session) {
       
       
       
-    titulo <- ifelse(input$titulo1=="","Ciclo anual",input$titulo1)
-    ejex <- ifelse(input$ejex1=="","Mes",input$ejex1)
-    ejey <- ifelse(input$ejey1=="","Altura",input$ejey1)
+    titulo <- ifelse(input$titulo1=="","Annual cycle",input$titulo1)
+    ejex <- ifelse(input$ejex1=="","Month",input$ejex1)
+    ejey <- ifelse(input$ejey1=="","Height",input$ejey1)
     
     
-    ggplot(data = MM, aes(x=Month, y=medias)) + 
+    ggplot(data = MM, aes(x=Month, y=medias)) + theme_minimal() +
       geom_boxplot(data = datos1,aes(x=Month,y=Y,group=Month), 
                    fill = "#aee7e8", outlier.color = "#24009c") + 
       scale_y_continuous(name = ejey) + 
@@ -151,7 +151,7 @@ shinyServer(function(input, output,session) {
                            c("Ene","Feb","Mar","Abr","May","Jun","Jul","Ago","Sep",
                              "Oct","Nov","Dic")) +
       labs(tag = titulo) + 
-      theme(plot.tag = element_text(lineheight = 2,face = "bold",size = 20),
+      theme(plot.tag = element_text(lineheight = 4,face = "bold",size = 20, hjust = 0.5),
             plot.tag.position = "top", axis.text.x = element_text(angle = 90))  +
       geom_point(color = "#c72c41")  + geom_line(color = "#c72c41")
       })
@@ -191,8 +191,8 @@ shinyServer(function(input, output,session) {
       datos2 <- as.data.frame(datos2)
       Quant <- data.frame(Probs=seq(0,1,0.0001),
                           Cuantil_P=quantile(datos2$datos2,probs = seq(0,1,0.0001)))
-      
-      Ajuste <- fitDist(y = datos2$datos2, type = "realplus",trace=FALSE)
+      tipo <- ifelse(min(datos2$datos2)<0,"realAll","realplus")
+      Ajuste <- fitDist(y = datos2$datos2, type = tipo,trace=FALSE)
       
     #eval = Evalua expresiones
     #parse = convierte textos en expresiones sin evaluar
@@ -204,17 +204,17 @@ shinyServer(function(input, output,session) {
     
     cuantiles <- round(quantile(Quant$Cuantil_P,c(0.01,0.05,0.95,0.99)),2)
     
-    titulo2 <- ifelse(input$titulo2=="",paste("Análisis de extremos: ",Ajuste$family[2]),input$titulo2)
-    ejex2 <- ifelse(input$ejex2=="","Datos",input$ejex2)
-    ejey2 <- ifelse(input$ejey2=="","Probabilidad acumulada",input$ejey2)
+    titulo2 <- ifelse(input$titulo2=="",paste("Analysis of extremes: ",Ajuste$family[2]),input$titulo2)
+    ejex2 <- ifelse(input$ejex2=="","Data",input$ejex2)
+    ejey2 <- ifelse(input$ejey2=="","Cumulative probability",input$ejey2)
     
-    ggplot(data = Quant) + 
+    ggplot(data = Quant) +  theme_minimal() +
       geom_line(aes(x = Cuantil_P, y = Probs), color = "#FF9770", size = 1.3) +
       geom_line(aes(x = Cuantil_T, y = Probs), color = "#70D6FF", size = 1.3) + 
-      scale_x_continuous(name = "X",breaks = seq(0,ceiling(max(datos2$datos2)),5)) +
-      scale_y_continuous(name = "Probability") +
-      labs(tag = paste("Data VS",Ajuste$family[2])) +
-      theme(plot.tag = element_text(lineheight = 2,face = "bold",size = 15),
+      scale_x_continuous(name = ejex2,breaks = seq(0,ceiling(max(datos2$datos2)),5)) +
+      scale_y_continuous(name = ejey2) +
+      labs(tag = titulo2) +
+      theme(plot.tag = element_text(lineheight = 4,face = "bold",size = 15, hjust = 0.5),
             plot.tag.position = "top", axis.text.x = element_text(angle = 90)) +
       scale_fill_manual(values = c("#FF9770","#70D6FF")) +
       geom_line(aes(x = Cuantil_T, y = 0.01), color = "#99b19c", size = 1) +
@@ -265,22 +265,23 @@ shinyServer(function(input, output,session) {
       datos<-dataset()
       datos2 <- datos[datos[,input$ycol] != -99,input$ycol]
       datos2 <- as.data.frame(datos2)
-      Ajuste <- fitDist(y = datos2$datos2, type = "realplus",trace=FALSE)
+      tipo <- ifelse(min(datos2$datos2)<0,"realAll","realplus")
+      Ajuste <- fitDist(y = datos2$datos2, type = tipo,trace=FALSE)
       datos2$densidad <- eval(parse(text = paste0("d",Ajuste$family[1],"(x = datos2$datos2, mu = ",
                                                  Ajuste$mu,", sigma = ",Ajuste$sigma,")")))
       
       
-  titulo3 <- ifelse(input$titulo3=="",paste("Mejor distribución: :",Ajuste$family[2]),input$titulo3)
-  ejex3 <- ifelse(input$ejex3=="","Datos",input$ejex3)
-  ejey3 <- ifelse(input$ejey3=="","Densidad",input$ejey3)
+  titulo3 <- ifelse(input$titulo3=="",paste("Best distribution :",Ajuste$family[2]),input$titulo3)
+  ejex3 <- ifelse(input$ejex3=="","Data",input$ejex3)
+  ejey3 <- ifelse(input$ejey3=="","Density",input$ejey3)
 
-  ggplot(data = datos2,aes(x = datos2)) + 
+  ggplot(data = datos2,aes(x = datos2)) + theme_minimal() +
     geom_histogram(aes(y = ..density..),binwidth=density(datos2$datos)$bw, fill = "#70D6FF",
                    color = "#000000") + 
     geom_line(aes(y = densidad), colour = "#FF9770", size = 1.3) + 
-    scale_x_continuous(name = "Data") + scale_y_continuous(name = "Density") +
-    labs(tag = paste("Density with distribution",Ajuste$family[2])) +
-    theme(plot.tag = element_text(lineheight = 2,face = "bold",size = 15),
+    scale_x_continuous(name = ejex3) + scale_y_continuous(name = ejey3) +
+    labs(tag = titulo3) +
+    theme(plot.tag = element_text(lineheight = 4,face = "bold",size = 15, hjust = 0.5),
           plot.tag.position = "top", axis.text.x = element_text(angle = 90))
   
       })#wihtprofgres
@@ -325,17 +326,17 @@ shinyServer(function(input, output,session) {
       
       
     
-    titulo4 <- ifelse(input$titulo4=="","Medias anuales",input$titulo4)
-    ejex4 <- ifelse(input$ejex4=="","Año",input$ejex4)
-    ejey4 <- ifelse(input$ejey4=="","Altura",input$ejey4)
+    titulo4 <- ifelse(input$titulo4=="","Annual averages",input$titulo4)
+    ejex4 <- ifelse(input$ejex4=="","Year",input$ejex4)
+    ejey4 <- ifelse(input$ejey4=="","Height",input$ejey4)
     
-    ggplot(data = MY, aes(x=Year, y=medias)) + 
+    ggplot(data = MY, aes(x=Year, y=medias)) + theme_minimal() +
       geom_boxplot(data = datos1,aes(x=Year,y=Y,group=Year), 
                    fill = "#aee7e8", outlier.color = "#24009c") +
       scale_y_continuous(name = ejey4) + 
       scale_x_continuous(name = ejex4, breaks = seq(min(MY$Year),max(MY$Year),1)) + 
       labs(tag = titulo4) + 
-      theme(plot.tag = element_text(lineheight = 2,face = "bold",size = 20),
+      theme(plot.tag = element_text(lineheight = 4,face = "bold",size = 20, hjust = 0.5),
             plot.tag.position = "top", axis.text.x = element_text(angle = 90))  +
       geom_point(color = "#c72c41") + geom_line(color = "#c72c41")
     
